5dcf757843d18f40c65d952529207e1b5e9ff03f
Christine <you@example.com>
2019-06-16 16:42:31 +0300

    adds display image function

--- mygallery/views.py
+++ mygallery/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Location,Category,Image
 
-# Create your views here.
+def welcome(request):
+    all_images = Image.objects.all()
+    category_results = Category.objects.all()
+    location_results = Location.objects.all()
+    return render(request,'index.html', {'all_images':all_images,'location_results':location_results,'category_results':category_results})
