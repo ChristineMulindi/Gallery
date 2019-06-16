7483256432a072db15cd166a6ab25dd4d159ad8e
Christine <you@example.com>
2019-06-16 16:59:41 +0300

    creates URLconfs

---
+++ mygallery/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns=[
+    url(r'^$',views.welcome,name='home-page'),
+    url(r'^search/',views.search_results, name='search_results'),
+    url(r'^category/(\w+)', views.get_category,name='get_category'),
+    url(r'^location/(\w+)', views.get_location,name='get_location'),
+
+]
