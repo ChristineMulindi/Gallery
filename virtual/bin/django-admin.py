58b58c44ae6dd4de8a4285099ae9622a91b54898
Christine <you@example.com>
2019-06-17 13:20:12 +0300

    fixes virtual problem

--- virtual/bin/django-admin.py
+++ virtual/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/christine/Desktop/Personal-Gallery/virtual/bin/python
+#!/home/christine/Desktop/Personal-Gallery/virtual/bin/python3
 from django.core import management
 
 if __name__ == "__main__":
